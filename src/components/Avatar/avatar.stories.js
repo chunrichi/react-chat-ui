8c55ecfa81effc6e1e579a3fc09d3aa93cc2b92c
旧事 <lei.hi@outlook.com>
2020-07-22 00:35:35 +0800

    课时15 jsconfig使用
    有的时候jsconfig更改后需要重启项目
    settings 中的checkJS会影响项目启动

--- src/components/Avatar/avatar.stories.js
+++ src/components/Avatar/avatar.stories.js
@@ -1,12 +1,12 @@
 import React from "react";
 import Avatar from ".";
 
-import face1 from "../../assets/images/face-male-1.jpg";
-import face2 from "../../assets/images/face-male-2.jpg";
-import face3 from "../../assets/images/face-male-3.jpg";
-import face4 from "../../assets/images/face-male-4.jpg";
+import face1 from "assets/images/face-male-1.jpg";
+import face2 from "assets/images/face-male-2.jpg";
+import face3 from "assets/images/face-male-3.jpg";
+import face4 from "assets/images/face-male-4.jpg";
 
-import '../../story.css'
+import 'story.css'
 
 export default {
   title: "Avatar",
