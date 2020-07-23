1b580ec0ee113fbcc74811327ba43d7133f703b3
旧事 <lei.hi@outlook.com>
2020-07-23 22:54:56 +0800

    课时24 按钮组件

---
+++ src/components/Button/button.stories.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Button from ".";
+import Icon from "components/Icon";
+
+import { ReactComponent as Plus } from "assets/icons/plus.svg";
+
+export default {
+  title: "UI 组件/Button",
+  component: Button,
+};
+
+export const RectButton = () => <Button shape="rect">默认</Button>;
+
+export const CircleButton = () => (
+  <Button>
+    <Icon icon={Plus} width={12} height={12} />
+  </Button>
+);
