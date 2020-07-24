550c384b463584b4e6262f00f70e0ca776a97f5f
旧事 <lei.hi@outlook.com>
2020-07-24 12:00:42 +0800

    课时25 过滤组件

---
+++ src/components/Filter/index.js
@@ -0,0 +1,29 @@
+import React from "react";
+import PropTypes from "prop-types";
+import StyledFilter, { Filters, Action } from "./style";
+import Text from "components/Text";
+
+function Filter({ children, ...rest }) {
+  return <StyledFilter {...rest}>{children}</StyledFilter>;
+}
+
+Filter.Filters = ({ children, label, ...rest }) => (
+  <Filters {...rest}>
+    <Text type="secondary">{label}</Text>
+    {children}
+  </Filters>
+);
+
+Filter.Action = ({ children, label, ...rest }) => (
+  <Action {...rest}>
+    <Text type="secondary">{label}</Text>
+    {children}
+  </Action>
+);
+
+// 属性规范
+Filter.propTypes = {
+  children: PropTypes.any,
+};
+
+export default Filter;
