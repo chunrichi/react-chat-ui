431d49be829e742982013aa9eedd1708ee2de783
旧事 <lei.hi@outlook.com>
2020-07-27 22:41:48 +0800

    课时50 屏蔽列表组件

---
+++ src/components/BlockedList/blockedList.stories.js
@@ -0,0 +1,11 @@
+import React from "react";
+import BlockedList from ".";
+
+export default {
+  title: "页面组件/BlockedList",
+  component: BlockedList,
+};
+
+export const Default = () => {
+  return <BlockedList>默认</BlockedList>;
+};
---
+++ src/components/BlockedList/index.js
@@ -0,0 +1,56 @@
+import React from "react";
+import PropTypes from "prop-types";
+import StyledBlockedList, {
+  SettingsMenu,
+  ClosableAvatar,
+  BlockedAvatar,
+  BlockedName,
+  CloseIcon,
+  FriendList,
+} from "./style";
+import Icon from "components/Icon";
+import Text from "components/Text";
+
+import "styled-components/macro";
+
+import { ReactComponent as ArrowMenuLeft } from "assets/icons/arrowMenuLeft.svg";
+import { ReactComponent as closeCircle } from "assets/icons/closeCircle.svg";
+import face from "assets/images/face-male-1.jpg";
+
+function BlockedList({ children, ...rest }) {
+  return (
+    <StyledBlockedList {...rest}>
+      <SettingsMenu>
+        <Icon
+          icon={ArrowMenuLeft}
+          css={`
+            cursor: pointer;
+          `}
+        />
+        <Text size="xxlarge">已屏蔽的好友</Text>
+      </SettingsMenu>
+      <FriendList>
+        {new Array(8).fill(0).map((_, i) => {
+          return <CloseAvatar avatarSrc={face} name="李浩" />;
+        })}
+      </FriendList>
+    </StyledBlockedList>
+  );
+}
+
+function CloseAvatar({ avatarSrc, name }) {
+  return (
+    <ClosableAvatar>
+      <BlockedAvatar size="105px" src={avatarSrc} />
+      <CloseIcon width={46} height={51} icon={closeCircle} />
+      <BlockedName>{name}</BlockedName>
+    </ClosableAvatar>
+  );
+}
+
+// 属性规范
+BlockedList.propTypes = {
+  children: PropTypes.any,
+};
+
+export default BlockedList;
