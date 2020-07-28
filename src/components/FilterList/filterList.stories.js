1d424b612daecac07c12b6a6629ebc2aeb343cba
旧事 <lei.hi@outlook.com>
2020-07-28 09:23:50 +0800

    课时51 抽离过滤组件

---
+++ src/components/FilterList/filterList.stories.js
@@ -0,0 +1,11 @@
+import React from "react";
+import FilterList from ".";
+
+export default {
+  title: "页面组件/FilterList",
+  component: FilterList,
+};
+
+export const Default = () => {
+  return <FilterList>默认</FilterList>;
+};
