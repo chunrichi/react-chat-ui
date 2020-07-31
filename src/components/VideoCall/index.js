41e0388119f35619785fe063b7cea82659541d8e
旧事 <lei.hi@outlook.com>
2020-07-31 19:02:14 +0800

    课时55 视频通话组件

---
+++ src/components/VideoCall/index.js
@@ -0,0 +1,95 @@
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+import StyledVideoCall, {
+  Minimize,
+  Actions,
+  Action,
+  Self,
+  VideoCallAlert,
+} from "./style";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faCompressAlt,
+  faMicrophone,
+  faPhoneSlash,
+  faVolumeMute,
+  faVideo,
+} from "@fortawesome/free-solid-svg-icons";
+import Avatar from "components/Avatar";
+import Paragraph from "components/Paragraph";
+
+import "styled-components/macro";
+
+import videoCaller from "assets/images/video-caller.jpg";
+import face from "assets/images/face-male-1.jpg";
+
+function VideoCall({ name = "李明", children, ...rest }) {
+  const [fullScreen, setFullScreen] = useState(true);
+
+  if (!fullScreen) {
+    return (
+      <VideoCallAlert>
+        <Avatar
+          src={face}
+          css={`
+            grid-area: avatar;
+          `}
+        />
+        <Paragraph
+          size="medium"
+          css={`
+            grid-area: info;
+          `}
+        >
+          正在和 {name} 进行视频通话
+        </Paragraph>
+        <Paragraph
+          type="secondary"
+          css={`
+            grid-area: action;
+            cursor: pointer;
+          `}
+          onClick={() => setFullScreen(true)}
+        >
+          点击切换全屏
+        </Paragraph>
+        <FontAwesomeIcon
+          icon={faVideo}
+          css={`
+            grid-area: icon;
+            font-size: 20px;
+            justify-self: end;
+            opacity: 0.3;
+          `}
+        />
+      </VideoCallAlert>
+    );
+  }
+
+  return (
+    <StyledVideoCall src={videoCaller} {...rest}>
+      <Minimize shape="rect" onClick={() => setFullScreen(false)}>
+        <FontAwesomeIcon icon={faCompressAlt} />
+      </Minimize>
+      <Actions>
+        <Action>
+          <FontAwesomeIcon icon={faMicrophone} />
+        </Action>
+        <Action type="hangoff">
+          <FontAwesomeIcon icon={faPhoneSlash} />
+        </Action>
+        <Action>
+          <FontAwesomeIcon icon={faVolumeMute} />
+        </Action>
+      </Actions>
+      <Self src={face} size="140px" />
+    </StyledVideoCall>
+  );
+}
+
+// 属性规范
+VideoCall.propTypes = {
+  children: PropTypes.any,
+};
+
+export default VideoCall;
---
+++ src/components/VideoCall/videoCall.stories.js
@@ -0,0 +1,15 @@
+import React from "react";
+import VideoCall from ".";
+
+export default {
+  title: "页面组件/VideoCall",
+  component: VideoCall,
+};
+
+export const Default = () => {
+  return (
+    <div style={{ height: "100vh" }}>
+      <VideoCall />
+    </div>
+  );
+};
